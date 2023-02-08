5956e0ab4703aec78e21c35a3c09acd9baed336d
ItsDimitrie <laue0002@HZ.nl>
2023-02-08 17:39:28 +0100

    Changed Landing page and Routing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,93 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-    >
-    <meta http-equiv="X-UA-Compatible"
-          content="ie=edge"
-    >
-    <title>{{ env('APP_NAME') }}</title>
-    {{-- Load the default generated javascript and stylesheets --}}
-    @vite([ 'resources/js/app.js', 'resources/sass/app.scss' ])
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Showcase Website Dimitrie Lauer</title>
 </head>
+
+<header>
+    <nav id="headerlinks">
+        <button class="active"> <a href="index.html">Home</a> </button>
+        <button> <a href="aboutme.html">About Me</a> </button>
+        <button> <a href="faq.html">FAQ</a></button>
+        <button> <a href="dashboard.html">Dashboard</a></button>
+        <button> <a href="blog.html">Blogs</a></button>
+    </nav>
+
+    <nav id="asidemenu" class="dropdown">
+        <button class="dropbtn">Handige Links!.</button>
+        <div class="dropdown-content">
+            <ul>
+                <li><a href="https://hz.nl/over-de-hz/regelingen-documenten-1/onderwijs-en-examenregelingen"
+                       target="_blank">Course and Examination Regulations</a></li>
+                <li><a href="https://glaze-donut-5a5.notion.site/Assignment-Specification-661c5ac5d7494328a58be61d00dd41e4"
+                       target="_blank">The Learn Assignment</a></li>
+                <li><a
+                        href="https://hz.nl/uploads/documents/1.4-Over-de-HZ/1.4.3.-Regelingen-en-documenten/OERS/2022-2023/IR-CER-HZ-B-HBO-ICT-full-time-2022-2023-DEF.pdf"
+                        target="_blank">The Implementation Regulations (IR)</a></li>
+                <li><a href="https://learn.hz.nl/my/" target="_blank">MyHZ Dashboard</a></li>
+                <li><a href="https://github.com/HZ-HBO-ICT" target="_blank">GitHub</a></li>
+                <li><a href="http://teams.microsoft.com/" target="_blank">MS Teams</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+
 <body>
-<nav class="navbar is-primary  has-text-white" >
-    <div class="container">
-        <div class="navbar-brand">
-            <a href="/" class="navbar-item">
-                {{-- Use the APP_NAME setting from the .env file --}}
-                <strong>{{ env('APP_NAME') }}</strong>
-            </a>
-            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
-        <div class="navbar-menu" id="navMenu">
-            <div class="navbar-start">
-                <a class="navbar-item" href="/posts/my-first-post">My first post</a>
-                <a class="navbar-item" href="/posts/my-second-post">My second post</a>
-            </div>
-        </div>
-    </div>
-</nav>
+<img src="./recources/img/cameraenik.png" id="headerfoto">
+<a href="https://hz.nl" target="_blank"> <img src="recources/img/hzlogomainpage.png" id="fotolinks"> </a>
+<img src="https://acegif.com/wp-content/uploads/2021/4fh5wi/welcome-5.gif" id="welkomgif">
+<main>
+    <div id="begintext">
+        <br>
+        <h2> Welcome to the home page of this website!</h2>
 
-<section class="section">
-    <div class="container">
-        <div class="content">
-            <h1>Welcome to my portfolio</h1>
-        </div>
+        <h3>This page was made with :</h3>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+        </ul>
     </div>
-</section>
 
+    <div id="flexbox">
+        <h3 id="welkomsbericht">This websie will be used as my personal website for all the HZ HBO-ICT Courses.
+            <br>
+            After being a cameraman for 4 years in the Media sector I have realised that altough the job is great and a lot of
+            fun I don't want to do it
+            untill my retirement. I have had an interest in Software en PC's from a younger age when I loved to game and build
+            computers.
+            The ICT education was therefore my most interesting education to pick and now I am attending it! Hence this
+            website. <br><br>
+
+            I think this education suits me because I have an interest for Computers and coding from when I was young. I used
+            to build PC's and edit my
+            own little lines of code in old Minecraft servers. Toghether with the motivation for the future I am sure this
+            will work out.<br><br>
+
+            This website is a showcase of the basic skills that I have for HTML and CSS!
+        </h3>
+    </div>
+
+
+    <img src="https://c.tenor.com/ZDixpmuHmpIAAAAC/i-hope-you-enjoy-your-stay-66samus.gif" id="welkomgif2">
+    <img src="https://c.tenor.com/TpWjyNO6wz8AAAAj/banana-dance.gif" id="bananaleft">
+    <img src="https://c.tenor.com/TpWjyNO6wz8AAAAj/banana-dance.gif" id="bananaright">
+</main>
+
+
+<footer id="socials">
+    <p>&copy; Dimitrie Lauer 2022</p>
+    <a href="https://www.facebook.com/dimitrie.lauer/" target="_blank">Facebook</a>
+    <a href="https://www.linkedin.com/in/dimitrie-lauer-b6270a14b/" target="_blank">LinkedIn</a>
+    <a href="https://www.instagram.com/its_dimah/" target="_blank">Instagram</a>
+    <a href="https://twitter.com/_Its_Dimah" target="_blank">Twitter</a>
+</footer>
 
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\WelcomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,22 +14,24 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//
+//Route::get('/posts/{post}',  function($post){
+//    $posts = [
+//        'my-first-post' => 'Hello, this is my first blog post!',
+//        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+//    ];
+//
+//    if (!array_key_exists($post, $posts)) {
+//        abort(404, 'Sorry, that post was not found.');
+//    }
+//
+//    return view('post', [
+//        'post' => $posts[$post]
+//    ]);
+//});
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('/posts/{post}',  function($post){
-    $posts = [
-        'my-first-post' => 'Hello, this is my first blog post!',
-        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-    ];
-
-    if (!array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found.');
-    }
-
-    return view('post', [
-        'post' => $posts[$post]
-    ]);
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'show']);
