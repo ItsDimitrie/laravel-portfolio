ebf78f54befd8f0dcf5c1143dde8f80aecc20966
ItsDimitrie <laue0002@HZ.nl>
2023-02-14 16:43:54 +0100

    Edited code quality after github feedback #4

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -19,21 +19,16 @@ class Grade extends Model
             $this->best_grade = $new_result;
 
             $this->save();
-            if ($this->best_grade >= $this->lowest_passing_grade)
-            {
+            if ($this->best_grade >= $this->lowest_passing_grade) {
                 $this->passed_at = now();
             }
-            else
-            {
+            else {
                 $this->passed_at = null;
             }
-
             $this->save();
         }
-        else
-        {
+        else {
             return;
         }
     }
 }
-
